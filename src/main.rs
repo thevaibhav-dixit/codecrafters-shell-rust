c92b51b534cdc7570311e08eb1b53867bb4948ba
Vaibhav <thevaibhavdixit@gmail.com>
2025-05-12 14:08:52 +0530

    trying repl logic

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+        // Wait for user inpu
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
-    println!("{}: command not found", input.trim());
+        println!("{}: command not found", input.trim());
+    }
 }
