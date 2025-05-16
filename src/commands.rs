48bf7b9e0c855243a9c2ed834e148fcffd7d411d
Vaibhav <thevaibhavdixit@gmail.com>
2025-05-17 00:27:31 +0530

    refactor: do not print output if there is output target

--- src/commands.rs
+++ src/commands.rs
@@ -121,8 +121,10 @@ impl Runnable for Binary {
                     }
                 }
 
-                if !output.stdout.is_empty() {
-                    print!("{}", String::from_utf8_lossy(&output.stdout));
+                if self.output_target.is_none() {
+                    if !output.stdout.is_empty() {
+                        print!("{}", String::from_utf8_lossy(&output.stdout));
+                    }
                 }
                 if !output.stderr.is_empty() {
                     eprint!("{}", String::from_utf8_lossy(&output.stderr));
